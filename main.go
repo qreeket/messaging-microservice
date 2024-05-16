a8752a5b9a08af09e44f95fd6d6c0b77ac9162cb
dennis-bilson <dennis@teksol.com.gh>
2024-05-16 20:26:24 +0000

    Updated admin

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"github.com/qcodelabsllc/qreeket/messaging/network"
+	"github.com/qcodelabsllc/qreeket/messaging/utils"
 	"log"
+	"strings"
 )
 
 func main() {
@@ -11,6 +14,19 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("unable to load environment variables: %+v\n", err)
 	}
+
+	// encrypt & decrypt message
+	key := []byte(strings.ReplaceAll(uuid.NewString(), "-", ""))
+	encryptedMessage, err := utils.EncryptMessage(key, "Hello World!")
+	if err != nil {
+		log.Fatalf("unable to encrypt message: %+v\n", err)
+	}
+	log.Printf("encrypted message: %s\n", string(encryptedMessage))
+	decryptedMessage, err := utils.DecryptMessage(key, encryptedMessage)
+	if err != nil {
+		log.Fatalf("unable to decrypt message: %+v\n", err)
+	}
+	log.Printf("decrypted message: %s\n", decryptedMessage)
 
 	// This line initializes the grpc server
 	network.InitServer()
